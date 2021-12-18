3f615511b1a0a3b538329f2e73d8a4eea21d6dcf
Lisovenko Anton <70dog@mail.ru>
2021-12-19 02:17:27 +0500

    data analysis methods

--- App/DataAnalyzer.py
+++ App/DataAnalyzer.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+from scipy.stats import norm, expon, poisson, binom, uniform
 import seaborn as sb
 import pandas
 from pandas.plotting import scatter_matrix
+import numpy as np
 
 class DataAnalyzer:
 
@@ -11,21 +13,32 @@ class DataAnalyzer:
     def analyse_data(self, data):
         pandas.set_option('display.max_columns', None)
         # print(data.dataframe.describe())
-        self.box_plot(data)
-        # self.plot_scatter_matrix(data)
-        self.plot_params_histograms(data)
-        self.plot_correlation_matrix(data)
+       # self.box_plot(data)
+        self.plot_scatter_matrix(data)
+        #self.plot_params_histograms(data)
+        self.plot_correlation_matrix(data.correlation_matrix)
 
     def plot_params_histograms(self, data):
-        data.dataframe.hist()
+        data.dataframe.hist(bins = 30)
         plt.show()
 
-    def plot_correlation_matrix(self, data):
-        plt.figure()
-        heatmap = sb.heatmap(data.correlation_matrix, cmap="coolwarm",
-                             vmin=-1, vmax=1, annot=True, linewidths=1,
-                             linecolor='black')
+    def plot_compare_hist_and_distribution(self, paramData):
+        sb.distplot(paramData, bins=40)
+        plt.show()
+
+    def plot_correlation_matrix(self, correlation_matrix):
+        fig, ax = plt.subplots(figsize=(10,10))
+        heatmap = sb.heatmap(correlation_matrix, cmap="coolwarm",
+                             vmin=-1, vmax=1, annot=True, linewidths=0.5,
+                             linecolor='black', ax=ax)
         heatmap.set_title('Correlation Heatmap')
+        plt.show()
+
+    def plot_p_values_matrix(self, matrix_dataframe):
+        fig, ax = plt.subplots(figsize=(10,10))
+        sb.heatmap(matrix_dataframe, ax=ax, annot = True, cbar=False,
+        cmap= 'coolwarm')
+        ax.set_title('P values matrix')
         plt.show()
 
     def box_plot(self, data):
@@ -34,5 +47,76 @@ class DataAnalyzer:
         plt.show()
 
     def plot_scatter_matrix(self, data):
-        scatter_matrix(data.dataframe, diagonal="kde")
+        fig, ax = plt.subplots(figsize=(4,4))
+        scatter_matrix(data.dataframe, diagonal="kde", ax=ax)
         plt.show()
+
+    def plot_correlation_statistic(self, data):
+        table_data, table_labels = self._get_correlation_statistic(data)    
+        fig, ax =plt.subplots(figsize=(10,10))
+        ax.xaxis.set_visible(False) 
+        ax.yaxis.set_visible(False)
+        table = ax.table(cellText=table_data,colLabels=table_labels,
+        loc="center")
+        #ax.set_title("Correlation statistic")
+        table.set_fontsize(14)
+        table.scale(2, 2)
+        plt.show()
+
+    def _get_correlation_statistic(self, data):
+        correlation_matrix, p_values_matrix = data.correlation_data
+        c_m_values = correlation_matrix.values
+        p_v_values = p_values_matrix.values
+        shape = correlation_matrix.shape
+        columns_names = correlation_matrix.columns
+        table_labels = ["Param 1 - Param 2", "Correlation coef (Power)",
+        "P value (Certainty)"]
+        table_data = []
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                if i >= j:
+                    continue             
+                params = "{0}-{1}:" \
+                .format(columns_names[i],columns_names[j])
+                row = [params,
+                self._get_correlation_description(c_m_values[i,j]),
+                self._get_P_value_description(p_v_values[i,j])]
+                table_data.append(row)
+                #table_data = np.append(table_data, row, axis=0)    
+        return table_data, table_labels
+
+    @staticmethod
+    def _get_P_value_description(p_value):
+        certainty = ""
+        if p_value < 0.001:
+            certainty = "Strong"
+        elif p_value < 0.05:
+            certainty = "Moderate"
+        elif p_value < 0.1:
+            certainty = "Weak"
+        elif p_value > 0.1:
+             certainty = "No"
+        return "{1} ({0})."\
+        .format(certainty, round(p_value,3))
+
+    @staticmethod
+    def _get_correlation_description(correlation_coef):
+        power = ""
+        correlation_coef = abs(correlation_coef)
+        if correlation_coef > 0.9:
+            power = "Very high"
+        elif correlation_coef > 0.7:
+            power = "High"
+        elif correlation_coef > 0.5:
+            power = "Moderate"
+        elif correlation_coef > 0.2:
+            power = "Low"
+        elif correlation_coef < 0.2:
+            power = "Very Low"
+
+        return "{0} ({1}).".format(round(correlation_coef,3), power)
+
+
+
+
+
--- App/PreProcessedData.py
+++ App/PreProcessedData.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.stats import pearsonr
+import pandas as pd
 
 
 class PreProcessedData:
@@ -17,8 +19,37 @@ class PreProcessedData:
         """
         float_data = self.dataframe.to_numpy()
         x, y = np.hsplit(float_data, [8])
-        return x,
+        return x, y
 
     @property
-    def correlation_matrix(self):
-        return self.dataframe.corr(method='pearson')
+    def correlation_data(self):
+        values = self.dataframe.values.T
+        columns_len = len(self.dataframe.columns)
+        correlation_matrix = np.empty((columns_len,columns_len), dtype=float)
+        p_values_matrix = np.empty((columns_len,columns_len), dtype=float)
+        for i, paramData1 in enumerate(values):
+            for j, paramData2 in enumerate(values):
+                if i > j:
+                    continue #симметричность матриц
+                corr = pearsonr(paramData1,paramData2)
+                correlation_matrix[i,j] = corr[0]
+                correlation_matrix[j,i] = corr[0]
+                p_values_matrix[i,j] = corr[1]
+                p_values_matrix[j,i] = corr[1]
+        df_corr_matrix = pd.DataFrame(
+            data = correlation_matrix,
+            columns=self.dataframe.columns,
+            index=self.dataframe.columns)
+        df_p_vals = pd.DataFrame(
+            data = p_values_matrix, 
+            columns=self.dataframe.columns, 
+            index=self.dataframe.columns)
+        return df_corr_matrix, df_p_vals
+
+
+       # return self.dataframe.corr(method='pearson')
+
+
+    @property
+    def kendall_correlation_matrix(self):
+        return self.dataframe.corr(method='kendall')
